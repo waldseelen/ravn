330b877f5cb1ee855fa7ad22e71795d5fa21d195
waldseelen <bugraakin01@gmail.com>
2026-08-05 23:08:39 +0300

    fix(frontend): Eliminate PrimeUI license warning by setting unstyled mode

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config'
-import Aura from '@primevue/themes/aura'
 import App from './App.vue'
 import { router } from './router'
 
@@ -9,10 +8,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(PrimeVue, {
-  theme: {
-    preset: Aura
-  }
-})
+app.use(PrimeVue, { unstyled: true })
 
 app.mount('#app')
